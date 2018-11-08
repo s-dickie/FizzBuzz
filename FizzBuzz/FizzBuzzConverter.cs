2f0966b34953c7a0ab9fde4ac0cb71acc2ba1c91
Saeed Dickie <saeeddickie@gmail.com>
2018-11-08 15:38:34 +0000

    Add project files.

---
+++ FizzBuzz/FizzBuzzConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+   public class FizzBuzzConverter
+    {
+
+
+        public string Implementation(int input)
+        {
+
+            int modularoutputdividebythree =  input % 3;
+            int modularoutputdividebyfive = input % 5;
+            string outputthree = "Fizz";
+            string outputfive = "Buzz";
+            string output = "";
+
+
+            if ((modularoutputdividebythree == 0) & (modularoutputdividebyfive == 0))
+            {
+                output = outputthree + outputfive;
+            }
+
+            else if ((modularoutputdividebythree == 0) & (modularoutputdividebyfive != 0))
+            {
+                output = outputthree;
+            }
+            else if ((modularoutputdividebythree != 0) & (modularoutputdividebyfive == 0))
+
+            {
+                output = outputfive;
+            }
+            else
+            {
+                output = input.ToString();
+            }
+                        
+            return output;
+
+            
+        }
+    }
+}
---
+++ FizzBuzzUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+
+namespace FizzBuzzUnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void DivisibleByThree()
+        {
+            FizzBuzzConverter FB = new FizzBuzzConverter();
+            int input = 3;
+            string output = FB.Implementation(input);
+
+            Assert.AreEqual("Fizz", output);
+        }
+
+        [TestMethod]
+        public void divisiblebyfive()
+        {
+            FizzBuzzConverter FB = new FizzBuzzConverter();
+            int input = 5;
+            string output = FB.Implementation(input);
+
+            Assert.AreEqual("Buzz", output);
+        }
+
+       [TestMethod]
+        public void divisiblebythreeandfive()
+        {
+            FizzBuzzConverter FB = new FizzBuzzConverter();
+            int input = 15;
+
+            string output = FB.Implementation(input);
+
+            Assert.AreEqual("FizzBuzz", output);
+        }
+
+         [TestMethod]
+        public void notdivisblebythreeorfive()
+        {
+            FizzBuzzConverter FB = new FizzBuzzConverter();
+            int input = 7;
+
+            string output = FB.Implementation(input);
+
+            Assert.AreEqual("7", output);
+        }
+
+
+    }
+}
